005bf1925cbf9807a8aca6f7efe19c28ebf93907
Oli <zelinkavo@gmail.com>
2026-07-27 15:40:55 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://olisoft.pages.dev',
 
   integrations: [
